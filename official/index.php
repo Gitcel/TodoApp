779cd38340e959d0432be33fdbe5d237f3d8e293
Ancel <ancel.fortuin@gmail.com>
2018-10-26 19:19:16 +0200

    Fixed the two bugs in the script and added something extra also.

--- official/index.php
+++ official/index.php
@@ -1,32 +1,88 @@
-<?php session_start(); ?>
+<?php
+
+    session_start();
+    $updatedTask = "";
+
+?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
+
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Todo app - Ancel</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="custom.css">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+
+    <title>Todo app - Ancel</title>
+
 </head>
 
 <body>
 
     <?php require('functions.php'); ?>
 
+    <?php 
+        
+        // What happens when adding a task, date ( with a countdown ). //
+        if(isset($_POST['submit']) && isset($_POST['todoItem'])) {
+            add_todo_item($_POST['todoItem']);
+            add_todo_date($_POST['todoDate']);
+            add_todo_countdown($_POST['todoDate']);          
+        }
+
+        // When a Remove button is clicked. //
+        if(isset($_POST['delete'])) {
+            delete_todo_item($_POST['delete']);
+        }
+
+        // When a status of the task is given. //
+        if(isset($_POST['complete'])) {
+            toggle_todo_item($_POST['complete']);
+        }
+        
+        // When the list of tasks are sorted. //        
+        if(isset($_POST['sort'])) {
+            sortByDate();
+        }
+    
+        // When an Edit button is clicked. //
+        if(isset($_POST['edit'])) {                      
+            $_SESSION['editKey'] = $_POST['edit'];            
+            echo "<br><h2 style='color: red;'>Edit button pressed. Key: ".$_SESSION['editKey']."</h2><br>";            
+        }
+
+        // When the Update button is clicked. //
+        if(isset($_POST['update'])) {
+            echo "<br><h2 style='color: red;'>Update button pressed. Key: ".$_SESSION['editKey']."</h2><br>";  
+            $updatedTask = $_POST['todoItem'];
+            $_SESSION['editDate'] = "";
+            
+            if (!empty($_POST['todoDate'])) {
+                $_SESSION['editDate'] = $_POST['todoDate'];
+                echo "<br><h2 style='color: red;'>Update button pressed. Date: ".$_SESSION['editDate']."</h2><br>";
+            }
+
+            update_todo_item($_SESSION['editKey'], $updatedTask, $_SESSION['editDate']);
+
+        }
+
+    ?>
+
     <div class="container">
 
         <h1 class="text-center">Tasks</h1>
 
         <form action="" method="post">
-
+            <!-- Check if the array is empty, before displaying anything in it. -->
             <?php if(!empty($_SESSION['todoItem'])) : ?>
 
                 <ul class="list-group">
-
+                    <!-- Display the tasks in the array. -->
                     <?php foreach($_SESSION['todoItem'] as $key => $item) : ?>
                         
                         <span class="date-class">
@@ -93,43 +149,6 @@
 
     </div>
 
-    <?php 
-    
-        // Add task, date & countdown button //
-        if(isset($_POST['submit']) && isset($_POST['todoItem'])) {
-            add_todo_item($_POST['todoItem']);
-            add_todo_date($_POST['todoDate']);
-            add_todo_countdown($_POST['todoDate']);          
-        }
-
-        // Remove button //
-        if(isset($_POST['delete'])) {
-            delete_todo_item($_POST['delete']);
-        }
-
-        // Status button //
-        if(isset($_POST['complete'])) {
-            toggle_todo_item($_POST['complete']);
-        }
-        
-        // Sort button //        
-        if(isset($_POST['sort'])) {
-            sortByDate();
-        }
-        // Update button //
-        if(isset($_POST['edit'])) {
-
-            $key = $_POST['edit'];
-        
-        }
-
-        if(isset($_POST['update'])) {
-            $updatedTask = $_POST['todoItem'];
-            update_todo_item($key, $updatedTask);
-        }
-        
-    ?>
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -139,6 +158,7 @@
         //unset($_SESSION['todoItem']);
         //print_r($_SESSION['todoItem']);
         //print_r($_POST['edit']);
+        //print_r($updatedTask);
         //print_r($_POST['todoItem']);
     ?></pre>
 
